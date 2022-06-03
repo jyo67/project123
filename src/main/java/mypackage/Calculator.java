7363e273f70dd759bb95ab5e5fc8bea057359c3d
jyo67 <102752891+jyo67@users.noreply.github.com>
2022-06-03 12:42:55 +0530

    Update Calculator.java

--- src/main/java/mypackage/Calculator.java
+++ src/main/java/mypackage/Calculator.java
@@ -39,11 +39,11 @@ public class Calculator extends HttpServlet
         }
         if(request.getParameter("r2")!=null)
         {
-            out.println("<h1>Substraction</h1>"-subFucn(a1, a2));
+            out.println("<h1>Substraction</h1>"+subFucn(a1, a2));
         }
         if(request.getParameter("r3")!=null)
         {
-            out.println("<h1>Multiplication</h1>"*mulFucn(a1, a2));
+            out.println("<h1>Multiplication</h1>"+mulFucn(a1, a2));
         }
         RequestDispatcher rd=request.getRequestDispatcher("/index.jsp");  
         rd.include(request, response);  
